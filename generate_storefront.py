6654d344cfd4e5d67d345070da6ab7889aea4d80
Dusty McLean <dusty_mclean@icloud.com>
2026-04-01 07:07:03 -0500

    Add GA4 tag to storefront

--- generate_storefront.py
+++ generate_storefront.py
@@ -1011,6 +1011,15 @@ def generate_site():
         html = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WK29KFSG7K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+
+      gtag('config', 'G-WK29KFSG7K');
+    </script>
     <meta charset="UTF-8">
     <title>{title} | Pixie's Pantry</title>
     <!-- Google Shopping / Merchant Center SEO -->
@@ -1090,6 +1099,15 @@ def generate_site():
         html = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WK29KFSG7K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+
+      gtag('config', 'G-WK29KFSG7K');
+    </script>
     <meta charset="UTF-8">
     <title>Community Power & Pricing | Pixie's Pantry</title>
     <link rel="stylesheet" href="css/style.css">
@@ -1191,6 +1209,15 @@ def generate_site():
     auth_html = """<!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WK29KFSG7K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+
+      gtag('config', 'G-WK29KFSG7K');
+    </script>
     <meta charset="UTF-8">
     <title>Account | Pixie's Pantry</title>
     <link rel="stylesheet" href="css/style.css">
